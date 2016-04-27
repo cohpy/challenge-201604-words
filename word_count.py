3b6f014d2610bbf359c9a8f9ecbe23c90a0827a2
cwandrews <cwandrews.oh@gmail.com>
2016-04-26 21:58:35 -0400

    Moved to uppercase in preperation to start using a file of english words... (not ideal but MVP is goal).

--- word_count.py
+++ word_count.py
@@ -5,15 +5,15 @@ from pprint import pprint
 
 from nltk.corpus import wordnet
 
-
 infile = '/Users/cwandrews/Downloads/pg84.txt'
 
 with open(infile, 'rt') as fh:
     text = fh.read()
-    lc_text = text.lower()
+    upcase_text = text.upper()
 
-word_reg = re.compile("[a-zA-Z]+'?")
-potential_matches = word_reg.findall(lc_text)
+word_reg = re.compile("[A-Z]+'?")
+
+potential_matches = word_reg.findall(upcase_text)
 
 potential_match_count = Counter(potential_matches)
 
@@ -21,8 +21,6 @@ word_list = list(potential_match_count.items())
 word_list.sort(key=lambda wc: wc[1], reverse=True)
 
 verified_words = [word for word in word_list if wordnet.synsets(word[0])]
-
-print(wordnet.synsets('i'))
 
 word_gen = (word for word in verified_words)
 
