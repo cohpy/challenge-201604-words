a07ef258bdb597f5756f4171f99594dd0cb4f547
cwandrews <cwandrews.oh@gmail.com>
2016-04-29 17:10:23 -0400

    Working with iteration (and much faster).

--- word_count.py
+++ word_count.py
@@ -2,6 +2,8 @@ import re
 from collections import Counter
 
 
+# TODO Trim pre and post book information (Gutenberg project information for example) from text to be analyzed
+
 # Target text to be read (Frankenstein)
 INFILE = './pg84.txt'
 # 230k+ words from the standard UNIX dict in a local text file ('/usr/share/dict/words')
@@ -12,43 +14,40 @@ def main():
 
     with open(INFILE, 'rt') as fh, open(ENGLISH_WORDS, 'rt') as ed:
         # Read the target text into a string translating all letters to lowercase
-        english_dict = sorted(list(set([eng_word.lower().rstrip('\n') for eng_word in ed.readlines()])))
+
+        blank_line_regex = re.compile("^\n$")
+        # word_regex = re.compile("[a-z]+\'?[a-z]+")
+        white_space_regex = re.compile("\s+")
+        special_chars_regex = re.compile("[-\"\':;.?!,\(\)\d]+")
+
+        # english_dict = sorted(list(set([eng_word.lower().rstrip('\n') for eng_word in ed.readlines()])))
         word_count = Counter()
 
-        for line in fh:
-            dcase_line = line.lower()
-            
+        for line in enumerate(line.rstrip('\n').strip() for line in fh.readlines() if not blank_line_regex.match(line)):
+            dcase_line = line[1].lower()
+            dcase_line = special_chars_regex.sub('', dcase_line)
 
-    '''
-        # List comprehension which strips new lines from words, gets rid of duplicates (with set), and resorts them
-        # alphabetically (for the sake of order)
+            line_matches = white_space_regex.split(dcase_line)
 
-
-    # Compile word regex to use for finding word-like structures
-    word_regex = re.compile("[a-z]+\'?[a-z]+")
-
-    # Find all matches of/for compiled regex
-    potential_matches = word_regex.findall(downcase_text)
-
-    # Reduce match list to any given match in the file and the number of instances determined
-    potential_match_count = Counter(potential_matches)
+            word_count.update(Counter(line_matches))
 
     # Create a list from counter object
-    word_list = list(potential_match_count.items())
+    word_list = list(word_count.items())
     # Sort list by word count in descending order
     word_list.sort(key=lambda wc: wc[1], reverse=True)
 
     # Check words against extensive list of English words, disregarding those not in the list
-    verified_words = [word for word in word_list if word[0] in english_dict]
+    # verified_words = [word for word in word_list if word[0] in english_dict]
 
     # Create a generator expression from list of verified words because I can
-    word_gen = (word for word in verified_words)
+    word_gen = (word for word in word_list)
 
     # Use genexp to print a formatted string of a word and the number of occurrences of said word in the text
     for word in word_gen:
         wc_format = "'{0!s}' * {1!s}"
         print(wc_format.format(word[0], word[1]))
-    '''
+
+    word_list[:10]
 
 if __name__ == '__main__':
     main()
