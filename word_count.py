a095d38330803e07e79b76ae44a6c5c4ab9cc641
cwandrews <cwandrews.oh@gmail.com>
2016-04-29 20:43:17 -0400

    Working with check that word is English and stripping of pre and post book material (Gutenberg information).

--- word_count.py
+++ word_count.py
@@ -15,29 +15,27 @@ def main():
     with open(INFILE, 'rt') as fh, open(ENGLISH_WORDS, 'rt') as ed:
         # Read the target text into a string translating all letters to lowercase
 
+        pre_post_text = re.compile("\n{10}")
         blank_line_regex = re.compile("^\n$")
-        # word_regex = re.compile("[a-z]+\'?[a-z]+")
         white_space_regex = re.compile("\s+")
         special_chars_regex = re.compile("[-\"\':;.?!,\(\)\d]+")
 
-        # english_dict = sorted(list(set([eng_word.lower().rstrip('\n') for eng_word in ed.readlines()])))
+        fh = pre_post_text.split(fh.read())[1]
+
+        english_dict = sorted(list(set([eng_word.lower().rstrip('\n') for eng_word in ed.readlines()])))
         word_count = Counter()
 
-        for line in enumerate(line.rstrip('\n').strip() for line in fh.readlines() if not blank_line_regex.match(line)):
+        for line in enumerate(line.strip() for line in fh.split('\n') if not blank_line_regex.match(line)):
             dcase_line = line[1].lower()
             dcase_line = special_chars_regex.sub('', dcase_line)
 
-            line_matches = white_space_regex.split(dcase_line)
-
-            word_count.update(Counter(line_matches))
+            word_count.update(Counter(white_space_regex.split(dcase_line)))
 
     # Create a list from counter object
-    word_list = list(word_count.items())
+    word_list = [word for word in word_count.items() if word[0] in english_dict]
+
     # Sort list by word count in descending order
     word_list.sort(key=lambda wc: wc[1], reverse=True)
-
-    # Check words against extensive list of English words, disregarding those not in the list
-    # verified_words = [word for word in word_list if word[0] in english_dict]
 
     # Create a generator expression from list of verified words because I can
     word_gen = (word for word in word_list)
@@ -47,7 +45,7 @@ def main():
         wc_format = "'{0!s}' * {1!s}"
         print(wc_format.format(word[0], word[1]))
 
-    word_list[:10]
+    print(word_list[:10])
 
 if __name__ == '__main__':
     main()
