814977264bd0d1b62aa6dbf13da9ff6597679c4c
cwandrews <cwandrews.oh@gmail.com>
2016-05-06 22:31:56 -0400

    Moved constants from source code file to the test file.

--- word_count.py
+++ word_count.py
@@ -4,15 +4,6 @@
 # TODO Add testing via pytest
 
 
-TARGET_STRING = 'This is? my file.\nIt is alright I suppose...\nThis is !really! just a test.\nI hope it, works'
-TARGET_STRING2 = 'This is just another string but longer and with no newlines to test the read_in_string method. is is.'
-# Target text to be read (Frankenstein)
-TARGET_FILE = './static_files/pg83.txt'
-TARGET_FILE2 = './static_files/pg84.txt'
-TEST_TXT = './static_files/test.txt'
-DOES_NOT_EXIST = './static_files/fake.txt'
-
-
 class WordCounter:
     """
     Read text from string or text file, counts words, and returns sorted list of tuples with the n most common words
