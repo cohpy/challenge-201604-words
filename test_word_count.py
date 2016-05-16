7907c3c7d3fbfd2e5c73d2e323d93d2bba648ea0
cwandrews <cwandrews.oh@gmail.com>
2016-05-15 21:08:25 -0400

    Removed redundant import statements.

--- test_word_count.py
+++ test_word_count.py
@@ -1,9 +1,8 @@
 # coding = utf-8
 # __author__ = cwandrews
-
 import pytest
 
-from word_count import WordCounter
+from word_count import WordCounter, LetterCounter
 
 
 TARGET_STRING = 'This is? my file.\nIt is alright I suppose...\nThis is !really! just a test.\nI hope it, works'
@@ -11,8 +10,6 @@ TARGET_STRING2 = 'This is just another string but longer and with no newlines to
 # Target text to be read (Frankenstein)
 TARGET_FILE = './static/pg83.txt'
 TARGET_FILE2 = './static/pg84.txt'
-TEST_TXT = './static/test.txt'
-DOES_NOT_EXIST = './static/fake.txt'
 
 
 @pytest.fixture("class")
@@ -34,7 +31,6 @@ def generator_words_dirty():
 class TestWordCounter:
 
     def test_char_counter_takes_str_gen_only(self):
-
         not_str_gens = tuple(), list(), dict(), set(), int(), str(), bytes(), float(), complex()
 
         for not_gen in not_str_gens:
@@ -44,7 +40,6 @@ class TestWordCounter:
         assert WordCounter._char_counter(generator_words_good(), num_words=5)
 
     def test_char_counter_returns_list_of_tuples_of_strings_and_counts_in_desc_order(self):
-
         counted_list = WordCounter._char_counter(generator_words_good(), num_words=5)
         counts_only = [obj[1] for obj in counted_list]
 
@@ -59,7 +54,6 @@ class TestWordCounter:
             assert counts_only[i] >= counts_only[i + 1]
 
     def test_char_counter_returns_no_non_english_words(self):
-
         english_words = './static/english_words.txt'
         with open(english_words, 'rt') as eng_dict:
             english_dict = list(set([eng_word.lower().rstrip('\n') for eng_word in eng_dict.readlines()]))
@@ -74,9 +68,7 @@ class TestWordCounter:
             assert word in english_dict
 
     def test_diff_n_words(self):
-        from word_count import WordCounter
-
-        n_words_tup = 15, 25, 35
+        n_words_tup = 15, 35
 
         for n_words in n_words_tup:
             word_count = WordCounter().read_in_file(filepath=TARGET_FILE, length=n_words)
@@ -86,8 +78,6 @@ class TestWordCounter:
 class TestLetterCounter:
 
     def test_diff_n_letters(self):
-        from word_count import LetterCounter
-
         n_letters_tup = 1, 26
 
         for n_letters in n_letters_tup:
@@ -95,8 +85,6 @@ class TestLetterCounter:
             assert len(letter_count) == n_letters
 
     def test_counts_letters_only(self):
-        from word_count import LetterCounter
-
         n_letters = 27
 
         with pytest.raises(StopIteration):
